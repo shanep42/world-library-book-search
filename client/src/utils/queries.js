a624bff7b87c45d14eac27833c4dbea22fe8413e
Shane <shanep42@gmail.com>
2022-12-11 15:08:34 -0600

    Adds front-end: utils/queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,22 @@
 // TODO: THis will hold the query GET_ME, which will execute the 'me' query set up using Apollo Server.
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        profiles {
+            _id
+            name
+            email
+            bookCount
+            savedBooks{
+                #_id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
