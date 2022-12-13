91bb4545581041a92e0197fb2493e2a8ae4a9794
Shane <shanep42@gmail.com>
2022-12-13 15:26:48 -0600

    Verifying most recent

--- server/server.js
+++ server/server.js
@@ -21,25 +21,28 @@
 // });
 
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
-
-const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+
+//Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
+
+const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
 });
 
-const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// TODO: Find out, is this standard practice/boilerplate, or case-specific? Can always remove the conditional
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')))
 }
