6f19bf79f3edb53a1495d1e8a0204dc2e197c126
Shane <shanep42@gmail.com>
2022-12-13 20:21:06 -0600

    Found and fixed error in resolvers.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -42,7 +42,7 @@ const SignupForm = () => {
       const { data } = await addUser({
         variables: { ...userFormData },
       });
-
+      console.log(data)
       Auth.login(data.addUser.token);
     } catch (err) {
       console.log(err, "HELLO");
